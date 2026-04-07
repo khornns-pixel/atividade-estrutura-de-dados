d9c09620ec2bc5cacb5e467d6388471e01447118
khornns-pixel <khornns@gmail.com>
2026-04-07 13:31:49 -0300

    Create exercicio1.c
    Inverter 3 palavras

---
+++ lista1/exercicio1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    char p1[20], p2[20], p3[20];
+
+    printf("Digite 3 palavras:\n");
+    scanf("%s %s %s", p1, p2, p3);
+
+    printf("Ordem inversa: %s %s %s\n", p3, p2, p1);
+
+    return 0;
+}
