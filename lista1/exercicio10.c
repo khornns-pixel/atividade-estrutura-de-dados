d53ea2b232d7818aec0d2fae12976f1d20b153a3
khornns-pixel <khornns@gmail.com>
2026-04-07 13:46:57 -0300

    Create exercicio10.c
    Maior nota (dinâmico)

---
+++ lista1/exercicio10.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Aluno {
+    char nome[30];
+    float nota;
+};
+
+struct Aluno* maiorNota(struct Aluno *a, int n) {
+    struct Aluno *maior = a;
+
+    for(int i = 1; i < n; i++) {
+        if((a + i)->nota > maior->nota) {
+            maior = (a + i);
+        }
+    }
+
+    return maior;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    struct Aluno *alunos = malloc(n * sizeof(struct Aluno));
+
+    for(int i = 0; i < n; i++) {
+        scanf("%s %f", (alunos + i)->nome, &(alunos + i)->nota);
+    }
+
+    struct Aluno *top = maiorNota(alunos, n);
+
+    printf("Maior nota: %s %.2f\n", top->nome, top->nota);
+
+    free(alunos);
+
+    return 0;
+}
