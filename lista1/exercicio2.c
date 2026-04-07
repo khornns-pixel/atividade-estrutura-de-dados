d84b1f2b3fdd18135a08cf1c3e1b33c819ad4030
khornns-pixel <khornns@gmail.com>
2026-04-07 13:33:55 -0300

    Create exercicio2.c
    Vetor invertido

---
+++ lista1/exercicio2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int v[5];
+
+    for(int i = 0; i < 5; i++) {
+        scanf("%d", &v[i]);
+    }
+
+    printf("Vetor invertido:\n");
+    for(int i = 4; i >= 0; i--) {
+        printf("%d ", v[i]);
+    }
+
+    return 0;
+}
