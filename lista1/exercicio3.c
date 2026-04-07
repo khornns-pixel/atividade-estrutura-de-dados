441cea0b90bee939fbebcca7f2d9e7ae990d9d4a
khornns-pixel <khornns@gmail.com>
2026-04-07 13:34:49 -0300

    Create exercicio3.c
    Matrix x5

---
+++ lista1/exercicio3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int m[3][3];
+
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 3; j++) {
+            scanf("%d", &m[i][j]);
+        }
+    }
+
+    printf("Matriz multiplicada por 5:\n");
+
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 3; j++) {
+            printf("%d ", m[i][j] * 5);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
