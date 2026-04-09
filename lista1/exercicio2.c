a90a38c7150175b33472e9bf2b45897b03b46b2d
khornns-pixel <khornns@gmail.com>
2026-04-08 22:03:50 -0300

    Update exercicio2.c
    Vetor invertido

--- lista1/exercicio2.c
+++ lista1/exercicio2.c
@@ -3,13 +3,14 @@
 int main() {
     int v[5];
 
+    printf("Digite 5 numeros:\n");
     for(int i = 0; i < 5; i++) {
         scanf("%d", &v[i]);
     }
 
     printf("Vetor invertido:\n");
     for(int i = 4; i >= 0; i--) {
-        printf("%d ", v[i]);
+        printf("%d", v[i]);
     }
 
     return 0;
