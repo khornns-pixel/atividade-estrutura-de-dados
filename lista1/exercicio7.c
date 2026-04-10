216833818f592c4750c418f155722a223347ee39
khornns-pixel <khornns@gmail.com>
2026-04-09 23:53:20 -0300

    Update exercicio7.c
    Buscar livro

--- lista1/exercicio7.c
+++ lista1/exercicio7.c
@@ -12,17 +12,26 @@ int main() {
     char busca[30];
 
     for(int i = 0; i < 5; i++) {
-        scanf("%s %s %d", livros[i].titulo, livros[i].autor, &livros[i].ano);
+        printf("Cadastrando livro %d de 5\n", i + 1);
+        printf("Digite o Titulo, o Autor e o Ano (separe por espaco): ");
+        scanf(" %s %s %d", livros[i].titulo, livros[i].autor, &livros[i].ano);
     }
 
-    printf("Digite o titulo: ");
-    scanf("%s", busca);
+    printf("\n--- Cadrasto Concluido! ---\n");
+    printf("Qual titulo voce quer buscar?");
+    scanf(" %s", busca);
+     printf("\n--- Resultado da busca ---\n");
+    int encontrado = 0;
 
     for(int i = 0; i < 5; i++) {
-        if(strcmp(livros[i].titulo, busca) == 0) {
-            printf("%s %s %d\n", livros[i].titulo, livros[i].autor, livros[i].ano);
+        if(strcmp(livros[i].titulo, busca) == 0) 
+        {
+            printf(" %s %s %d\n", livros[i].titulo, livros[i].autor, livros[i].ano);
+            encontrado = 1;
         }
     }
 
+    if(!encontrado) printf("Nenhum livro encontrado com esse titulo.\n");
+    
     return 0;
 }
